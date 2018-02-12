4fdb17421fc875bc03e9d78009b6a042d75f9c1d
eahkub <36217230+eahkub@users.noreply.github.com>
2018-02-12 15:47:31 +0700

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,1 +1,22 @@
+import sys
 
+day_seq = 0
+day_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+month = raw_input('Input month: ')
+
+if (int(month)>12) or (int(month)<1):
+    print 'In correct month.'
+    sys.exit()
+
+date = raw_input('Enter date: ')
+
+
+if (int(date)>day_in_month[int(month)-1]) or int(date)<1:
+    print 'Incorrect date.'
+    sys.exit()
+
+for x in range(0, int(month)-1):
+    day_seq += day_in_month[x]
+
+print 'Day sequence of the year is %d' % (int(day_seq)+int(date))
